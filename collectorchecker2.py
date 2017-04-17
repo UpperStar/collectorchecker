dadbef0910f1cfb7a5acd720bfb7eb3a57c32e45
UpperStar <upper.star@aol.com>
2017-04-17 09:36:57 +0200

    Add files via upload

---
+++ collectorchecker2.py
@@ -0,0 +1,31 @@
+import httplib2
+import os
+from termcolor import colored, cprint
+clear = lambda: os.system('cls')
+clear()
+cprint('\n    ███    █▄     ▄███████▄    ▄███████▄    ▄████████    ▄████████    ▄████████     ███        ▄████████    ▄████████ \n'
+'    ███    ███   ███    ███   ███    ███   ███    ███   ███    ███   ███    ███ ▀█████████▄   ███    ███   ███    ███ \n'
+'    ███    ███   ███    ███   ███    ███   ███    █▀    ███    ███   ███    █▀     ▀███▀▀██   ███    ███   ███    ███ \n'
+'    ███    ███   ███    ███   ███    ███  ▄███▄▄▄      ▄███▄▄▄▄██▀   ███            ███   ▀   ███    ███  ▄███▄▄▄▄██▀ \n'
+'    ███    ███ ▀█████████▀  ▀█████████▀  ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   ▀███████████     ███     ▀███████████ ▀▀███▀▀▀▀▀   \n'
+'    ███    ███   ███          ███          ███    █▄  ▀███████████          ███     ███       ███    ███ ▀███████████ \n'
+'    ███    ███   ███          ███          ███    ███   ███    ███    ▄█    ███     ███       ███    ███   ███    ███ \n'
+'    ████████▀   ▄████▀       ▄████▀        ██████████   ███    ███  ▄████████▀     ▄████▀     ███    █▀    ███    ███ \n'
+'                                                        ███    ███                                         ███    ███ \n', 'green')
+cprint('---------> JVC Collector Checker V1.0\n', 'green', 'on_red')
+
+def check(pseudo):
+	c = httplib2.Http('.cache')
+	url = 'http://www.jeuxvideo.com/profil/' + pseudo + '?mode=infos'
+	resp, content = c.request(url, 'GET', )
+	if resp.status == 200:
+		cprint(pseudo + ' est indisponible', 'red')
+	else:
+		cprint(pseudo + ' est disponible', 'green')
+
+path = input('Chemin d\'accès du fichier ?')
+cprint('Checking...', 'yellow')
+with open(path) as f:
+	for pseudo in f:
+		check(pseudo.rstrip('\n'))
+	
